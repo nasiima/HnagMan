9af15a379d9da648b2ef095ad46163940e2ac187
Nasiima Mohamed <nasiima.mohamed1@gmail.com>
2023-11-14 14:22:41 +0000

    task 2 check wether the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,12 @@
 
+import random
+
+
+word_list = ["apple", "banana", "orange", "grape", "kiwi"]
+
+
+secret_word = random.choice(word_list)
+
 while True:
 
     guess = input("Guess a letter: ")
@@ -12,3 +20,9 @@ while True:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
 
+if guess in secret_word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
+print(f"The word was: {secret_word}")
