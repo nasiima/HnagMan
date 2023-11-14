0a58bfc9925b563d792ba5359b0f35e7ecb62670
Nasiima Mohamed <nasiima.mohamed1@gmail.com>
2023-11-14 14:56:13 +0000

    refactor

--- milestone_4.py
+++ milestone_4.py
@@ -19,16 +19,16 @@ class Hangman:
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             
-            # Loop through each letter in the word
+
             for i in range(len(self.word)):
                 letter = self.word[i]
                 
-                # Check if the letter is equal to the guess
+
                 if letter == guess:
-                    # Replace the corresponding "_" in the word_guessed with the guess
+   
                     self.word_guessed[i] = guess
                     
-            # Reduce the variable num_letters by 1
+
             self.num_letters -= 1
         else:
             print(f"Sorry, {guess} is not in the word.")
