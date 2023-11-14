e5b8154efde1e827f62f919312fb26c85a8ff61c
Nasiima Mohamed <nasiima.mohamed1@gmail.com>
2023-11-14 14:47:57 +0000

    create methods for running the checks

--- milestone_4.py
+++ milestone_4.py
@@ -2,18 +2,42 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        # Step 3
+ 
         self.word_list = word_list
-        self.word = random.choice(word_list)
-        self.word_guessed = ['_' for _ in self.word]
+        self.word = self._get_random_word()
+        self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.list_of_guesses = []
 
-# Testing the Hangman class
-hangman_game = Hangman(["apple", "banana", "orange", "grape", "kiwi"])
-print(f"Word to guess: {hangman_game.word}")
-print(f"Word guessed so far: {hangman_game.word_guessed}")
-print(f"Number of unique letters: {hangman_game.num_letters}")
-print(f"Number of lives: {hangman_game.num_lives}")
-print(f"List of guesses: {hangman_game.list_of_guesses}")
+    def _get_random_word(self):
+  
+        return random.choice(self.word_list)
+    
+    def check_guess(self, guess):
+      
+        guess = guess.lower() 
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word.")
+    
+ 
+
+    def ask_for_input(self):
+
+        while True:
+            guess = input("Guess a letter: ").lower()
+            if not guess.isalpha() or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)  
+                self.list_of_guesses.append(guess)  
+                break  
+
+# Example usage:
+word_list = ['apple', 'banana', 'orange', 'grape', 'kiwi']
+hangman_game = Hangman(word_list)
+hangman_game.ask_for_input()
